1673149001cb205c9f6c3e7e54f54bc0af6b4761
1 <1@COMPUTER>
2023-08-27 19:42:19 +0500

    Добавьте файлы проекта.

---
+++ OtusDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class OtusDictionary
+    {
+        private static int _count = 32;
+        private string[] _storage = new string[_count];
+        private bool _examinationTrue;
+        public void Add(int key, string value)
+        {
+            foreach (var item in _storage)
+            {
+                if (item == null)
+                {
+                    _examinationTrue = false;
+                }
+            }
+            if (_examinationTrue == true)
+            {
+                throw new OccupancyException("Массив заполнен");
+            }
+            _examinationTrue = true;
+            int hash = key % 32;
+            if (_storage[hash] == null)
+            {
+                _storage[hash] = value;
+            }
+            else
+            {
+                if (_storage[hash] != value)
+                {
+                    _count = _count * 2;
+                    Resize(key, value);
+                }
+                else
+                {
+                    _storage[hash] = value;
+                }
+            }
+        }
+        public string Get(int key)
+        {
+            Console.WriteLine($"Под индексом {key} находится {_storage[key]}");
+            return _storage[key];
+        }
+        public void Resize(int key, string value)
+        {
+            string[] storage1 = new string[_count];
+            for (int i = 0; i < _storage.Length; i++)
+            {
+                if (_storage[i] != null)
+                {
+                    int hash1 = i % storage1.Count();
+                    storage1[hash1] = _storage[i];
+                }
+            }
+            _storage = storage1;
+            Add(key, value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OtusDictionary otusDictionary = new OtusDictionary();
+            otusDictionary.Add(1, "п");
+            otusDictionary.Add(2, "р");
+            otusDictionary.Add(3, "и");
+            otusDictionary.Add(4, "в");
+            otusDictionary.Add(5, "е");
+            otusDictionary.Add(6, "т");
+            otusDictionary.Add(7, "п");
+            otusDictionary.Add(8, "р");
+            otusDictionary.Add(9, "и");
+            otusDictionary.Add(10, "в");
+            otusDictionary.Add(11, "е");
+            otusDictionary.Add(12, "т");
+            otusDictionary.Add(13, "к");
+            otusDictionary.Add(14, "аи");
+            otusDictionary.Add(15, "к");
+            otusDictionary.Add(16, "тп");
+            otusDictionary.Add(17, "п");
+            otusDictionary.Add(18, "т");
+            otusDictionary.Add(19, "те");
+            otusDictionary.Add(20, "ради");
+            otusDictionary.Add(21, "тес");
+            otusDictionary.Add(22, "теста");
+            otusDictionary.Add(23, "е");
+            otusDictionary.Add(24, "в");
+            otusDictionary.Add(25, "м");
+            otusDictionary.Add(26, "с");
+            otusDictionary.Add(27, "к");
+            otusDictionary.Add(28, "м");
+            otusDictionary.Add(29, "й");
+            otusDictionary.Add(30, "м");
+            otusDictionary.Get(31);
+        }
+    }
+}
